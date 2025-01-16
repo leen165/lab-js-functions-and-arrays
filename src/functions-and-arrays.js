709ec2c678268a2a179b765f4501479b7a7c712e
Leen Zien <Loli@Mac.fritz.box>
2025-01-16 17:25:06 +0100

    finished all

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,14 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+
+
+function maxOfTwoNumbers(x,y) {
+    if(x<y){
+     return y;
+    }else{
+     return x;
+    }
+}
+
 
 
 
@@ -7,7 +16,23 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+    if(arr==0) {
+        return null;
+    }
+  let longestWord = arr[0];
+  
+  for( let i = 1; i<arr.length; i++){
+    if(arr[i].length > longestWord.length){
+        longestWord = arr[i];
+    }
+  }
+  console.log(longestWord);
+  return longestWord;
+
+}
+findLongestWord(words);
+
 
 
 
@@ -15,7 +40,19 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+    let result=0;
+  if (arr==""){
+    return 0;
+  }else if(arr.length === 1)
+    return arr[0];
+  
+   for(let i=0; i < arr.length; i++){
+       result += arr[i];
+   }
+  return result;
+}
+console.log(sumNumbers(numbers));
 
 
 
@@ -23,12 +60,35 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+    if(arr==""){
+        return 0;
+    }
+  let sum = 0;
+  for(let i=0; i < arr.length; i++){
+    sum+= arr[i];
+  }
+
+  return(sum/arr.length);
+}
 
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const search = "leen";
+function doesWordExist(arr, searchWord) {
+    if(arr =="")
+        return null;
 
-function doesWordExist() {}
+
+  for(let i=0; i < arr.length; i++){
+       if(arr.includes(searchWord)){
+        return true;
+       }else{
+        return false;
+       }
+  }
+}
+console.log(doesWordExist(words2, search));
